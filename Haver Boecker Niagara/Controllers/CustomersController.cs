9da1e0d925a657b6acf119bb4df429fa54c0015a
Ahri <damion.jakob@gmail.com>
2025-01-18 20:21:31 -0500

    Paging, filtering, and sorting for customers

--- Haver Boecker Niagara/Controllers/CustomersController.cs
+++ Haver Boecker Niagara/Controllers/CustomersController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Haver_Boecker_Niagara.Data;
 using Haver_Boecker_Niagara.Models;
+using Haver_Boecker_Niagara.Utilities;
+using Haver_Boecker_Niagara.CustomControllers;
 
 namespace Haver_Boecker_Niagara.Controllers
 {
-    public class CustomersController : Controller
+    public class CustomersController : ElephantController
     {
         private readonly HaverContext _context;
 
@@ -20,9 +22,131 @@ namespace Haver_Boecker_Niagara.Controllers
         }
 
         // GET: Customers
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string? SearchName, string? SearchContact,
+            string? SearchPhone, string? SearchEmail, int? page, int? pageSizeID, string? actionButton,
+            string sortDirection = "asc", string sortField = "Customer")
         {
-            return View(await _context.Customers.ToListAsync());
+            //List of sort options
+            string[] sortOptions = new[] { "Name", "ContactPerson", "PhoneNumber", "Email" };
+
+            //Number of filters applied
+            ViewData["Filtering"] = "btn-outline-secondary";
+            int numberFilters = 0;
+
+            var customers = _context.Customers
+                .AsNoTracking();
+
+            if (!String.IsNullOrEmpty(SearchName))
+            {
+                customers = customers.Where( c => c.Name.Contains(SearchName));
+                numberFilters++;
+            }
+            if (!String.IsNullOrEmpty(SearchContact))
+            {
+                customers = customers.Where( c=> c.ContactPerson.Contains(SearchContact));
+                numberFilters++;
+            }
+            if (!String.IsNullOrEmpty(SearchPhone))
+            {
+                customers = customers.Where( c => c.PhoneNumber.Contains(SearchPhone));
+                numberFilters++;
+            }
+            if (!String.IsNullOrEmpty(SearchEmail))
+            {
+                customers = customers.Where( c => c.Email.Contains(SearchEmail));
+                numberFilters++;
+            }
+            //Give feedback about the state of the filters
+            if (numberFilters != 0)
+            {
+                //Toggle the Open/Closed state of the collapse depending on if we are filtering
+                ViewData["Filtering"] = " btn-danger";
+                //Show how many filters have been applied
+                ViewData["numberFilters"] = "(" + numberFilters.ToString()
+                    + " Filter" + (numberFilters > 1 ? "s" : "") + " Applied)";
+                //Keep the Bootstrap collapse open
+                @ViewData["ShowFilter"] = " show";
+
+            }
+
+            //See if we have called for a change of filtering or sorting
+            if (!String.IsNullOrEmpty(actionButton))
+            {
+                page = 1;//Reset page to start
+
+                if (sortOptions.Contains(actionButton))//Change of sort is requested
+                {
+                    if (actionButton == sortField) //Reverse order on same field
+                    {
+                        sortDirection = sortDirection == "asc" ? "desc" : "asc";
+                    }
+                    sortField = actionButton;//Sort by the button clicked
+                }
+            }
+
+            //Handling sorting
+            if (sortField == "Name")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(c => c.Name);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(c => c.Name);
+                }
+            }
+            else if (sortField == "ContactPerson")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(c => c.Email);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(c => c.Email);
+                }
+            }
+            else if (sortField == "PhoneNumber")
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(c => c.PhoneNumber);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(c => c.PhoneNumber);
+                }
+            }
+            else //Sorting by email
+            {
+                if (sortDirection == "asc")
+                {
+                    customers = customers
+                        .OrderBy(c => c.Email);
+                }
+                else
+                {
+                    customers = customers
+                        .OrderByDescending(c => c.Email);
+                }
+            }
+            //Setting sort for next time
+            ViewData["sortField"] = sortField;
+            ViewData["sortDirection"] = sortDirection;
+
+            //Handle Paging
+            int pageSize = PageSizeHelper.SetPageSize(HttpContext, pageSizeID, ControllerName());
+            ViewData["pageSizeID"] = PageSizeHelper.PageSizeList(pageSize);
+            var pagedData = await PaginatedList<Customer>.CreateAsync(customers.AsNoTracking(), page ?? 1, pageSize);
+
+            return View(pagedData);
         }
 
         // GET: Customers/Details/5
