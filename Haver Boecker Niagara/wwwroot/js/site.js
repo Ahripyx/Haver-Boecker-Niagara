5037ef29e9d1fcefa50ba4dc4d53641d3d2f24fc
Gamaliel <chamba123g@gmail.com>
2025-01-26 14:47:54 -0600

    Adding toast notifications prototype 1 working fine waiting for form validations -Gamaliel Romualdo

--- Haver Boecker Niagara/wwwroot/js/site.js
+++ Haver Boecker Niagara/wwwroot/js/site.js
@@ -7,3 +7,93 @@
 /*//////////////////////////////////////
     Toast Notifications 
 *///////////////////////////////////////
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    const page = document.body.dataset.page;
+    const btnCreate = document.getElementById('btnCreate');
+    const btnDelete = document.getElementById('btnDelete');
+    const btnEdit = document.getElementById('btnEdit');
+    const notification = document.querySelector('.notifications')
+    const toastSucess = document.querySelector('.toast.success');
+    const toastError = document.querySelector('.toast.error');
+    const toastWarning = document.querySelector('.toast.warning');
+    const toastInfo = document.querySelector('.toast.info');
+
+    //Remove the toast by clicking X
+    const removeToast = (toast) => {
+        toas.style.display = 'none';
+        toastError.style.display = 'none';
+        toastWarning.style.display = 'none';
+        toastInfo.style.display = 'none';
+        setTimeout(() => toastSucess.remove(), 500);
+        setTimeout(() => toastError.remove(), 500);
+        setTimeout(() => toastWarning.remove(), 500);
+        setTimeout(() => toastInfo.remove(), 500);
+    }
+
+    if (btnCreate) {
+        console.log('Btn exist')
+        btnCreate.addEventListener("click", () => {
+            sessionStorage.setItem("showToast", "true");
+            const inputs = document.querySelectorAll('input');
+            let error = false;
+            inputs.forEach(i => {
+                if (!i.value.trim()) {
+                    error = true;
+                }
+            })
+            if (error) {
+                if (toastError) {
+                    toastError.style.display = 'flex';
+                    setTimeout(() => {
+                        toastError.style.display = "none";
+                    }, 5000);
+                }
+            } 
+        });
+    }
+    if (btnEdit) {
+        console.log('Btn exist')
+        btnEdit.addEventListener("click", () => {
+            sessionStorage.setItem("showToast", "true"); 
+            const inputs = document.querySelectorAll('input');
+            let error = false;
+            inputs.forEach(i => {
+                if (!i.value.trim()) {
+                    error = true;
+                }
+            })
+            if (error) {
+                if (toastError) {
+                    toastError.style.display = 'flex';
+                    setTimeout(() => {
+                        toastError.style.display = "none";
+                    }, 5000);
+                }
+            } 
+        });
+    }
+    if (btnDelete) {
+        console.log('Btn exist')
+        btnDelete.addEventListener("click", () => {
+            sessionStorage.setItem("showToast", "true");
+        });
+    }
+
+    const showToast = sessionStorage.getItem("showToast");
+    
+    if (showToast === "true") {
+        const toast = document.querySelector('.toast.success');
+        if (toast) {
+            toast.style.display = "flex";
+
+            sessionStorage.removeItem("showToast");
+
+           
+            setTimeout(() => {
+                toast.style.display = "none";
+            }, 5000);
+        }
+    }
+   
+})
