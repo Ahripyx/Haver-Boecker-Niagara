e2a2a93fa0629a3fa8a4e1e22a122a93a1ce5aee
Gamaliel <chamba123g@gmail.com>
2025-03-02 15:42:15 -0600

    Adding style for gantt schecule

---
+++ Haver Boecker Niagara/ViewModels/GanttTimeLineViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Haver_Boecker_Niagara.ViewModels
+{
+    public class GanttTimeLineViewModel
+    {
+        public int GanttID { get; set; }
+        public int SalesOrderID { get; set; }
+        public bool EngineeringOnly { get; set; }
+        public List<MilestoneViewModel> Milestones { get; set; } = new List<MilestoneViewModel>();
+    }
+    public class MilestoneViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
