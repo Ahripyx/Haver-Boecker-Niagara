b4bf9f3989568abc3a9fa6205c994efcd33eb132
Ahripyx <ahripyx@gmail.com>
2025-03-24 20:02:14 -0400

    Fixed identity roles not being created, and login seed data so far - Ahri

--- Haver Boecker Niagara/Data/ApplicationDbInitializer.cs
+++ Haver Boecker Niagara/Data/ApplicationDbInitializer.cs
@@ -52,6 +52,46 @@ namespace Haver_Boecker_Niagara.Data
                 }
             }
             #endregion
+
+            #region Seed Users
+            if (SeedSampleData)
+            {
+                using (var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>())
+                {
+                    try
+                    {
+                        var users = new[]
+                        {
+                            new { UserName = "admin@haver.com", Password = "Admin@123", Role = "Admin" },
+                            new { UserName = "sales@haver.com", Password = "Sales@123", Role = "Sales" },
+                            new { UserName = "engineering@haver.com", Password = "Engineering@123", Role = "Engineering" },
+                            new { UserName = "procurement@haver.com", Password = "Procurement@123", Role = "Procurement" },
+                            new { UserName = "production@haver.com", Password = "Production@123", Role = "Production" },
+                            new { UserName = "pic@haver.com", Password = "PIC@123", Role = "PIC" },
+                            new { UserName = "readonly@haver.com", Password = "ReadOnly@123", Role = "Read Only" }
+                        };
+
+                        foreach (var user in users)
+                        {
+                            var existingUser = await userManager.FindByEmailAsync(user.UserName);
+                            if (existingUser == null)
+                            {
+                                var identityUser = new IdentityUser { UserName = user.UserName, Email = user.UserName };
+                                var result = await userManager.CreateAsync(identityUser, user.Password);
+                                if (result.Succeeded)
+                                {
+                                    await userManager.AddToRoleAsync(identityUser, user.Role);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.GetBaseException().Message);
+                    }
+                }
+            }
+            #endregion
         }
     }
 
